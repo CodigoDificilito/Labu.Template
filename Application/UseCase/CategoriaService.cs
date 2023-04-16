eb4733d365d1328af7303ac50ff015610c0a354b
diego <diego@DESKTOP-4M1EFDE>
2023-04-15 22:21:31 -0300

    Agregar archivos de proyecto.

---
+++ Application/UseCase/CategoriaService.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces.Commands;
+using Application.Interfaces.Services;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCase
+{
+    public class CategoriaService : ICategoriaService
+    {
+        private readonly ICategoriaCommand _command;
+        //private readonly ICategoriaQuery _query;
+
+        public CategoriaService(ICategoriaCommand command /* , ICategoriaQuery query  */)
+        {
+            _command = command;
+            //_query = query
+        }
+
+        public Task<Categoria> CreateCategoria()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Labu/Controllers/CategoriaController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Labu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly ICategoriaService _service;
+
+        public CategoriaController(ICategoriaService service)
+        {
+            _service = service;
+        }
+
+        //[httpget]
+    }
+}
